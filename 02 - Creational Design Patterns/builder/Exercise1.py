ec1671905e80e812bba47914a482cbac15614a0a
mousa nageh <200moussa200@gmail.com>
2024-03-07 22:13:41 +0200

    add exercises to builder

---
+++ 02 - Creational Design Patterns/builder/Exercise1.py
@@ -0,0 +1,56 @@
+'''
+Create a Builder pattern implementation for a pizza ordering system. 
+The goal is to allow customers to build their custom pizza by choosing different ingredients like the crust type, sauce, cheese, and toppings.
+'''
+
+from abc import ABC, abstractmethod
+
+class Pizza:
+    
+    def __init__(self):
+        self.name = ''
+        self.type = None 
+        self.price = None 
+    
+    @staticmethod
+    def build():
+        return PizzaBuild()
+    
+    def __str__(self) -> str:
+        return f'name : {self.name}, type : {self.type}, price: {self.price}'
+        
+
+class PizzaBuildABC(ABC):
+    
+    def __init__(self) -> None:
+        self.pizza = Pizza()
+    
+    @abstractmethod
+    def set_name(self, value): ...
+    
+    @abstractmethod
+    def set_type(self, value): ...
+    
+    @abstractmethod
+    def set_price(self, value): ...
+    
+    def build(self):
+        return self.pizza
+    
+    
+
+class PizzaBuild(PizzaBuildABC):
+    
+    def set_name(self, value):
+        self.pizza.name = value
+        return self 
+    
+    def set_price(self, value):
+        self.pizza.price = value
+        return self 
+    
+    def __str__(self) -> str:
+        return str(self.pizza)
+
+pizze = Pizza.build().set_name("pizza 1").set_price(12).set_type('sauce').build()
+print(pizze)
---
+++ 02 - Creational Design Patterns/builder/Exercise2.py
@@ -0,0 +1,75 @@
+'''
+Design a Builder pattern system for constructing different types of cars.
+A car's configuration could include the 
+1 - engine type (electric, petrol, diesel), 
+2 - the color, 
+3 - the number of seats, 
+4 - and optional features like a sunroof or advanced navigation systems. 
+'''
+
+from abc import ABC
+
+class Car:
+
+    def __init__(self):
+        self.engine = None 
+        self.color = None
+        self.seats = None 
+        self.features = []
+    
+    def __str__(self) -> str:
+        return f"engine :{self.engine}, color: {self.color}, number of seats:{self.seats}  features : {self.features}" 
+    
+    @staticmethod
+    def build():
+        return CarBuider()
+    
+     
+
+class CarBuilderABC(ABC):
+    
+    def __init__(self, car: Car = None):
+        self.car = Car() if not car else car
+    
+    
+
+class CarBuider(CarBuilderABC):
+    
+    @property
+    def base(self):
+        return BaseCarBuilder(car=self.car)
+    
+    @property
+    def handle_features(self):
+        return CarFeatureBuilder(car=self.car)
+
+
+class BaseCarBuilder(CarBuider):
+    
+    def set_engine(self, engine):
+        self.car.engine = engine 
+        return self
+    
+    def set_numuber_of_seats(self, seats):
+        self.car.seats = seats 
+        return self
+    
+    def set_color(self, color):
+        self.car.color = color
+        return self
+    
+
+class CarFeatureBuilder(CarBuider):
+    
+    def add_feature(self, feature):
+        self.car.features.append(feature)
+        return self
+    
+    def remove_feature(self, index):
+        del self.car.features[index]
+        return self
+
+build_car = Car.build().base.set_color('red').set_engine('electric').set_numuber_of_seats(4)
+build_car.handle_features.add_feature('feature 1')
+
+print(build_car.car)
